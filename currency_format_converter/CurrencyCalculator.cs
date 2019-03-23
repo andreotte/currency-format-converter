9374bed3698b8e313411e7bed596d4f596df245c
andre <andre.b.otte1@gmail.com>
2019-03-23 16:14:56 -0400

    Created program
    Added classes, methods, and user input. Need to format outputs.

---
+++ currency_format_converter/CurrencyCalculator.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace currency_format_converter
+{
+    public class CurrencyCalculator
+    {
+        //Parameters
+        private readonly double[] _values;
+
+        //Constructor
+        public CurrencyCalculator(double[] values)
+        {
+            _values = values;
+        }
+        //Method to find total
+        public double ReturnTotal() 
+        {
+            double total = 0.0;
+            for (int i = 0; i < _values.Length; i++)
+            {
+                total += _values[i];
+            }
+            return (double)total;
+        }
+
+        //Method to find average
+        public double ReturnMean()
+        {
+            double total = 0.0;
+            for (int i = 0; i < _values.Length; i++)
+            {
+                total += _values[i];
+            }
+            return (double)total / _values.Length;
+        }
+
+        //Method to find latgest
+        public double ReturnLargest()
+        {
+            double largest = 0;
+            for (int i = 0; i < _values.Length; i++)
+            {
+                if(_values[i] > largest) 
+                {
+                    largest = _values[i];
+                }
+            }
+            return largest;
+        }
+
+        //Method to find smallest
+        public double ReturnSmallest()
+        {
+            double smallest = _values[0];
+            for (int i = 0; i < _values.Length; i++)
+            {
+                if (_values[i] < smallest)
+                {
+                    smallest = _values[i];
+                }
+            }
+            return smallest;
+        }
+    }
+}
---
+++ currency_format_converter/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+
+namespace currency_format_converter
+{
+    class Converter
+    {
+        public static void Main()
+        {
+            // Array to hold dollar amounts
+            double[] amounts = new double[3];
+            Console.WriteLine("Enter a dollar amount:");
+            int i = 0;
+
+            while (amounts.Contains(0)) 
+            {
+                Console.Write("$");
+                try
+                {
+                    double amount = Convert.ToDouble(Console.ReadLine());
+                    amounts[i] = amount;
+                    i += 1;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Please enter a dollar amount formatted as: 1.00");
+                    continue;
+                }
+            }
+
+            // New instance of CurrencyCalculator 
+            CurrencyCalculator calculator = new CurrencyCalculator(amounts);
+
+            // Call ReturnTotal method
+            double total = calculator.ReturnTotal();
+            // Call ReturnMean method
+            double mean = calculator.ReturnMean();
+            // Call ReturnLargest method
+            double largest = calculator.ReturnLargest();
+            // Call ReturnSmallest method
+            double smallest = calculator.ReturnSmallest();
+
+            Console.WriteLine("The total cost is: $" + total);
+            Console.WriteLine("The average cost of the items is: $" + Math.Round(mean, 2));
+            Console.WriteLine("The most expensive item is: $" + largest);
+            Console.WriteLine("The least expensive item is: $" + smallest);
+        }
+    }
+}
